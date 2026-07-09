894313760bd90d47f3efb0c53e1e6517ddb91f27
ABDUL HAIY <abdulhaiy1502@gmail.com>
2026-07-09 15:32:35 +0530

    Time: 3 ms (11.83%), Space: 19.5 MB (53.96%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,8 +1,15 @@
 class Solution:
+    def findpow(self, x, n):
+        if n == 0:
+            return 1
+        a = self.findpow(x, n//2)
+        if n%2 == 0:
+            return a*a
+        else:
+            return a*a*x
     def myPow(self, x: float, n: int) -> float:
         if -100 < x < 100 or (-2)**31 <= n <= 2**31:
-            if n == 0:
-                return 1
-            if n == 1:
-                return x
-            return x**n
+            if n >= 0:
+                return self.findpow(x, n)
+            else:
+                return 1/(self.findpow(x, n*(-1)))
