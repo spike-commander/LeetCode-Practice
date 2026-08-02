9cb1c228d84a34659c702207b2a890ec684ca84e
ABDUL HAIY <abdulhaiy1502@gmail.com>
2026-08-02 14:46:14 +0530

    Time: 7 ms (0.66%), Space: 17.4 MB (0.66%) - LeetHub

--- 0877-stone-game/0877-stone-game.c
+++ 0877-stone-game/0877-stone-game.c
@@ -1,13 +1,45 @@
 #include <stdbool.h>
+#include <stdlib.h>
 
 /**
- * Predicts if Alice will win the stone game.
+ * Predicts if Alice will win the stone game using Dynamic Programming.
+ * Works for the general case of the game.
  * 
  * @param piles     An array representing the number of stones in each pile.
- * @param pilesSize The total number of piles (guaranteed to be even).
+ * @param pilesSize The total number of piles.
  * @return          true if Alice wins, false otherwise.
  */
 bool stoneGame(int* piles, int pilesSize) {
-    // Alice always wins by choosing either all odd or all even piles.
-    return true;
+    // Dynamically allocate a 2D array for the DP table
+    int** dp = (int**)malloc(pilesSize * sizeof(int*));
+    for (int i = 0; i < pilesSize; i++) {
+        dp[i] = (int*)malloc(pilesSize * sizeof(int));
+    }
+
+    // Base case: Single piles
+    for (int i = 0; i < pilesSize; i++) {
+        dp[i][i] = piles[i];
+    }
+
+    // Fill the table for subarrays of length 2 to pilesSize
+    for (int length = 2; length <= pilesSize; length++) {
+        for (int i = 0; i <= pilesSize - length; i++) {
+            int j = i + length - 1;
+            int pickLeft = piles[i] - dp[i + 1][j];
+            int pickRight = piles[j] - dp[i][j - 1];
+            
+            dp[i][j] = (pickLeft > pickRight) ? pickLeft : pickRight;
+        }
+    }
+
+    // The result represents Alice's score minus Bob's score
+    int netScoreDifference = dp[0][pilesSize - 1];
+
+    // Free allocated memory
+    for (int i = 0; i < pilesSize; i++) {
+        free(dp[i]);
+    }
+    free(dp);
+
+    return netScoreDifference > 0;
 }
